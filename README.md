de81d79c16608e141b948caeeac9d8bc66f6b6dc
Ruth John <Rumyra@Ruths-MacBook.local>
2016-07-27 21:55:25 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # demo-boilerplate
-Node boilerplate for demos/pocs
+
+Node boilerplate for demos/pocs loosely based on [Jack Franklin's](https://twitter.com/Jack_Franklin) Express boilerplate [found here](https://github.com/jackfranklin/express-server-boilerplate).
+
+It's stripped down, only really for one/two page demos/apps, to get up and running quickly.
---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const htmling = require('htmling');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT || 3001);
